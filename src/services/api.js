b8f098082d96d55a2a32ae84621a6dde58c6bbb9
bateradt <marcelofscarpim@gmail.com>
2020-01-28 21:36:44 -0300

    Corrigido URL do Heroku para acesso a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://backend-mapsdevs.herokuapp.com'//'http://localhost:3004'
+    baseURL: 'https://backend-mapsdevs.herokuapp.com'//'http://localhost:3004'
 });
 
 export default api;
