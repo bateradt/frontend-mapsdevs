5f10286f44b4fe0e5e6e07620ef0de4a0d3199d2
bateradt <marcelofscarpim@gmail.com>
2020-01-28 21:26:36 -0300

    Inserido URL do Heroku para acesso a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3004'
+    baseURL: 'http://backend-mapsdevs.herokuapp.com'//'http://localhost:3004'
 });
 
 export default api;
